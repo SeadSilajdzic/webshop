2a5639bdbf66cda7c7cd413760d930c75a7907f8
Sead Silajdzic <sead.silajdzic@elevatebits.com>
2024-03-13 10:44:08 +0100

    money formatting

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Money\Currency;
+use Money\Money;
 
 class Product extends Model
 {
@@ -22,5 +25,14 @@ class Product extends Model
     public function images(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Image::class);
+    }
+
+    public function price(): Attribute
+    {
+        return Attribute::make(
+            get: function(int $value) {
+                return new Money($value, new Currency('USD'));
+            }
+        );
     }
 }
