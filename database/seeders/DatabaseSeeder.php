ff3321dc6280ea142e9892e7133b7d0be7f94726
Sead Silajdzic <sead.silajdzic@elevatebits.com>
2024-03-13 12:56:53 +0100

    persist cart model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Image;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
@@ -19,5 +20,9 @@ class DatabaseSeeder extends Seeder
             ->hasProductVariants(5)
             ->has(Image::factory(3)->sequence(fn(Sequence $sequence) => ['featured' => $sequence->index === 0])) // Make first image featured
             ->create();
+
+        User::factory()->create([
+            'email' => 'admin@admin.com'
+        ]);
     }
 }
