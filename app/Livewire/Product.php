25596757fdb247cbdbd373922334dd3841a6e55d
Sead Silajdzic <sead.silajdzic@elevatebits.com>
2024-03-13 11:21:59 +0100

    add to cart action validation

--- app/Livewire/Product.php
+++ app/Livewire/Product.php
@@ -7,6 +7,21 @@ use Livewire\Component;
 class Product extends Component
 {
     public int $productId;
+    public mixed $variant;
+
+    public array $rules = [
+        'variant' => ['required', 'exists:App\Models\ProductVariant,id']
+    ];
+
+    public function mount()
+    {
+        $this->variant = $this->product->productVariants()->value('id');
+    }
+
+    public function addToCart()
+    {
+        $this->validate();
+    }
 
     public function getProductProperty()
     {
--- resources/views/livewire/product.blade.php
+++ resources/views/livewire/product.blade.php
@@ -22,13 +22,17 @@
         </div>
 
         <div class="mt-4 space-y-4">
-            <select class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800" id="">
+            <select wire:model="variant" class="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-800" id="">
                 @foreach($this->product->productVariants as $variant)
                     <option value="{{ $variant->id }}">{{ $variant->size }} / {{ $variant->color }}</option>
                 @endforeach
             </select>
 
-            <x-button>Add to cart</x-button>
+            @error('variant')
+                <div class="mt-2 text-red-600">{{ $message }}</div>
+            @enderror
+
+            <x-button wire:click="addToCart">Add to cart</x-button>
         </div>
     </div>
 </div>
